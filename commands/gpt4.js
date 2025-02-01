afae557dd7b3256dd75e8d391bf5d2c2687daddd
nigfuhjfxtg <ffthy1678@gmail.com>
2025-02-01 17:22:11 +0100

    Update gpt4.js

--- commands/gpt4.js
+++ commands/gpt4.js
@@ -3,7 +3,7 @@ const { sendMessage } = require('../handles/sendMessage');
 
 module.exports = {
   name: 'gpt4',
-  description: 'Interact with GPT-4o',
+  description: 'Interact with Gemini API',
   usage: 'gpt4 [your message]',
   author: 'coffee',
 
@@ -12,8 +12,8 @@ module.exports = {
     if (!prompt) return sendMessage(senderId, { text: "Usage: gpt4 <question>" }, pageAccessToken);
 
     try {
-      const { data: { response } } = await axios.get(`https://api.kenliejugarap.com/blackbox-gpt4o/?text=${encodeURIComponent(prompt)}`);
-      sendMessage(senderId, { text: response }, pageAccessToken);
+      const { data } = await axios.get(`http://sgp1.hmvhostings.com:25721/gemini?question=${encodeURIComponent(prompt)}`);
+      sendMessage(senderId, { text: data.response || data }, pageAccessToken);
     } catch {
       sendMessage(senderId, { text: 'There was an error generating the content. Please try again later.' }, pageAccessToken);
     }
